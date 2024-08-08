2fa8a78e47bfebcf699d8e5f637087d94926ab02
Enomahus <enomahus@outlook.com>
2024-08-08 23:37:46 +0200

    MIse à jour html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'github-ci';
+  title = 'github-ci demo pipeline ci/cd';
 }
